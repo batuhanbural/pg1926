3653efb615315060e07df9fead0b97a5b1c0b95c
Batuhan BURAL <60934501+batuhanbural@users.noreply.github.com>
2021-02-09 17:40:01 +0300

    Delete python_problem_set_4.py

--- python_problem_set_4.py
+++
@@ -1,5 +0,0 @@
-num_list = input("Sayıları aralarına virgül koyarak yazınız.").split(",")
-newn_list = [int(i) for i in num_list]
-newn_list.sort(reverse=True)
-newn_list = [i for i in newn_list if i % 2 == 1]
-print(newn_list[0])
